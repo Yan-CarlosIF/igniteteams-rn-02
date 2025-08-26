8d1ae4e0526c07ed5afb14bbde2382c65e18f263
Yan-CarlosIF <yan312005@gmail.com>
2025-08-26 00:17:12 -0300

    feat: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { BackButton, BackIcon, Container, Logo } from "./styles";
+import LogoImg from "@assets/Logo.png";
+
+type HeaderProps = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: HeaderProps) {
+  return (
+    <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
+      <Logo source={LogoImg} />
+    </Container>
+  );
+}
