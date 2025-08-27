f0739046d1f6979fd22d7fdc9c6d1cb810f90bf5
Yan-CarlosIF <yan312005@gmail.com>
2025-08-27 01:22:36 -0300

    feat: new GroupCard component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { CaretLeft } from "phosphor-react-native";
+import { CaretLeftIcon } from "phosphor-react-native";
 
 export const Container = styled.View`
   width: 100%;
@@ -18,7 +18,7 @@ export const BackButton = styled.TouchableOpacity`
   flex: 1;
 `;
 
-export const BackIcon = styled(CaretLeft).attrs(({ theme }) => ({
+export const BackIcon = styled(CaretLeftIcon as any).attrs(({ theme }) => ({
   size: 32,
   color: theme.COLORS.WHITE,
 }))``;
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,18 @@
 import { Header } from "@/components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@/components/Highlight";
+import { GroupCard } from "@/components/GroupCard";
 
 export function Groups() {
+  const handleTest = () => {
+    console.log("test");
+  };
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <GroupCard onPress={handleTest} title="Lenda" />
     </Container>
   );
 }
