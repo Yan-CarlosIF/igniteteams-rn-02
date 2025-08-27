e6b7a98dcf0c260ba25cabbcead409f600f7dee7
Yan-CarlosIF <yan312005@gmail.com>
2025-08-27 14:43:35 -0300

    feat: playerCard component and Players screen

---
+++ src/components/PlayerCard/index.tsx
@@ -0,0 +1,17 @@
+import { ButtonIcon } from "../ButtonIcon";
+import { Container, Icon, Name } from "./styles";
+
+type PlayerCardProps = {
+  name: string;
+  onRemove: () => void;
+};
+
+export function PlayerCard({ name, onRemove }: PlayerCardProps) {
+  return (
+    <Container>
+      <Icon name="person" />
+      <Name>{name}</Name>
+      <ButtonIcon onPress={onRemove} icon="close" type="SECONDARY" />
+    </Container>
+  );
+}
--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,9 +6,20 @@ import { ButtonIcon } from "@/components/ButtonIcon";
 import { Filter } from "@/components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
+import { PlayerCard } from "@/components/PlayerCard";
+import { ListEmpty } from "@/components/ListEmpty";
+import { Button } from "@/components/Button";
 
 export function Players() {
-  const [players, setPlayers] = useState<string[]>(["Rodrigo"]);
+  const [players, setPlayers] = useState<string[]>([
+    "Rodrigo",
+    "Yan",
+    "Diego",
+    "Guilherme",
+    "Thiago",
+    "Maria",
+    "Natalia",
+  ]);
   const [selectedTeam, setSelectedTeam] = useState("Time A");
 
   return (
@@ -40,6 +51,26 @@ export function Players() {
         />
         <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+        showsVerticalScrollIndicator={false}
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard
+            onRemove={() => console.log(`removendo ${item}`)}
+            name={item}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+      />
+      <Button title="Remover turma" type="SECONDARY" />
     </Container>
   );
 }
